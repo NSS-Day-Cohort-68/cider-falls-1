661e8676aefebd26ff5edb9e1deaf1b054dfbd3f
Claire Hanson <clairecatohanson@gmail.com>
2023-11-30 13:46:27 -0600

    refactor to accomodate database.js

--- scripts/ServicesList.js
+++ scripts/ServicesList.js
@@ -1,23 +1,23 @@
-// import { getServices } from './main.js'  <-- remove comment when database.js is available
+import { getServices } from './database.js'
 
 // temporary services array to be used while waiting for access to the database:
-const services = [
-    {id: 1, service: 'Rafting'},
-    {id: 2, service: 'Canoeing'},
-    {id: 3, service: 'Fishing'},
-    {id: 4, service: 'Hiking'},
-    {id: 5, service: 'Picnicking'},
-    {id: 6, service: 'Rock Climbing'},
-    {id: 7, service: 'Lodging'},
-    {id: 8, service: 'Parking'},
-    {id: 9, service: 'Information'},
-    {id: 10, service: 'Zip Lines'}
-]
+// const services = [
+//     {id: 1, service: 'Rafting'},
+//     {id: 2, service: 'Canoeing'},
+//     {id: 3, service: 'Fishing'},
+//     {id: 4, service: 'Hiking'},
+//     {id: 5, service: 'Picnicking'},
+//     {id: 6, service: 'Rock Climbing'},
+//     {id: 7, service: 'Lodging'},
+//     {id: 8, service: 'Parking'},
+//     {id: 9, service: 'Information'},
+//     {id: 10, service: 'Zip Lines'}
+// ]
 
 // function to provide the html string for the services component
 // invoked in main.js within the render() function
 export const ServicesHTML = () => {
-    // const services = getServices()   <-- remove comment when database.js is available 
+    const services = getServices()
     let servicesHtml = '<h2>Park Services</h2><ul>'
     for (const service of services) {
         servicesHtml += `<li data-id="${service.id}">${service.service}</li>`
