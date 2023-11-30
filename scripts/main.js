76487c70624e33d66baa9e72b6182dde3b38d02c
Emily Mai <emily.vy.mai@gmail.com>
2023-11-30 14:00:35 -0600

    adding park areas and services

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 import { ServicesHTML } from './ServicesList.js'
+import { AreaServicesHTML } from './ParkAreas.js'
 
 const render = () => {
     const container = document.getElementById('container')
 
     container.innerHTML = `
-    <div id='services-list'></div>
-    ${ServicesHTML()}
-    <div id='locations-list'></div>
+    <div id='services-list'>${ServicesHTML()}</div>
+    <div id='locations-list'>${AreaServicesHTML()}</div>
     <div id='guests-list'></div>
 `
 }
