1871544bd3a143a2e1607b9eecb21f4a898bd01f
Eric Heidel <ericleeheidel@yahoo.com>
2023-11-30 13:52:26 -0600

    Added guestlist.js & modified main.js to accomodate

---
+++ scripts/guestlist.js
@@ -0,0 +1,21 @@
+import { getGuests } from "./database.js"
+
+const allGuests = getGuests()
+
+export const GuestsList = () => {
+
+    let guestsListHTML = "<ul>"
+
+    for (const guest of allGuests) {
+        guestsListHTML += `
+                        <li>${guest.firstName} ${guest.lastName}</li>`
+    }
+
+    guestsListHTML += "</ul>"
+    
+    return guestsListHTML
+
+}
+
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
+import { GuestsList } from "./guestlist.js"
+
 const render = () => {
     const container = document.getElementById('container')
 
     container.innerHTML = `
     <div id='services-list'></div>
     <div id='locations-list'></div>
-    <div id='guests-list'></div>
+    <div id='guests-list'>
+    ${GuestsList()}
+    </div>
 `
 }
 
