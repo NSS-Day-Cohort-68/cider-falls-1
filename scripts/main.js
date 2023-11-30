1d36ae9a06233e383d1a9307b99bde95515a2e62
Hewson Schulz <hewsonschulz@gmail.com>
2023-11-30 10:50:04 -0600

    initial render preparation

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const render = () => {
+    const container = document.getElementById('container')
+
+    container.innerHTML = `
+    <div id='services-list'></div>
+    <div id='locations-list'></div>
+    <div id='guests-list'></div>
+`
+}
+
+render()
