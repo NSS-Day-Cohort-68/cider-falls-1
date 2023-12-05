03926fc57812acb65db82c8c25477025d825398f
Hewson Schulz <hewsonschulz@gmail.com>
2023-12-05 10:06:34 -0600

    fixed ParkAreas rendering bug

--- scripts/ParkAreas.js
+++ scripts/ParkAreas.js
@@ -1,51 +1,56 @@
-import { getParkAreas, getServices, getAreaServices } from "../database/database.js"
+import { getParkAreas, getServices, getAreaServices } from '../database/database.js'
 
 const parkAreas = getParkAreas()
 const services = getServices()
 const areaServices = getAreaServices()
 
-//find the service for an areaService
-const findService = (areaServices, allServices) => {
-    let areaServService = null
 
-    for (const service of allServices) {
-        if (service.id === areaServices.parkId) {
-            areaServService = service
+//pair correct areas to services
+const linkAreaServices = (areas, services, areaServices) => {
+    let out = []
+    // loop through each areaService
+    for (const pairing of areaServices) {
+        // loop through each area
+        for (const area of areas) {
+            // if the area matches, loop through each service
+            if (pairing.parkId === area.id) {
+                for (const service of services) {
+                    // if the service matches, add the pairing to the output
+                    if (pairing.serviceId === service.id) {
+                        out.push({
+                            location: area.location,
+                            service: service.service
+                        })
+                    }
+                }
+            }
         }
     }
-    return areaServService
+
+    // returns an array of pairing objects
+    return out
 }
 
-//find the park area for an areaService
-const findParkArea = (areaService, allParkAreas) => {
-    let areaServ = null
-
-    for (const park of allParkAreas) {
-        if (park.id === areaService.parkId) {
-            areaServ = park
-        }
-    }
-    return areaServ
-}
 
 export const AreaServicesHTML = () => {
-    let html = `<section class="parkAreasCard">`
+    let html = `<section class='parkAreasCard'>`
+    const pairings = linkAreaServices(parkAreas, services, areaServices)
 
-    for (const aservice of areaServices) {
+    for (const area of parkAreas) {
+        html += `<h3>${area.location}</h3><ul>`
 
-        const area = findParkArea(aservice, parkAreas)
-        const service = findService(aservice, services)
-
-        html += `<h3>${area.location}</h3>
-                <ul>`
-        if (area.id === aservice.parkId && service.id === aservice.serviceId) {
-            html += `<li class="services">
-                        ${service.service}
-                        </li>`
+        for (const pairing of pairings) {
+            if (pairing.location === area.location) {
+                html += `<li class='services'>
+                ${pairing.service}
+                </li>`
+            }
         }
+
+        html += '</ul>'
     }
 
-    html += "</ul></section>"
+    html += '</section>'
 
     return html
 }
